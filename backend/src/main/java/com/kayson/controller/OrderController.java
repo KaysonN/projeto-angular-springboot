72214d570c5cdde793337f9339179eac84daa71f
KaysonN <carloskayson@hotmail.com>
2022-08-19 23:07:11 -0300

    Criando pedido(Order) e salvando no banco de dados via HTTP POST - implementação da lógica para que isso funcione

--- backend/src/main/java/com/kayson/controller/OrderController.java
+++ backend/src/main/java/com/kayson/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.kayson.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kayson.model.Order;
@@ -20,5 +24,11 @@ public class OrderController {
 	@GetMapping
 	public List<Order> list() {
 		return orderRepository.findAll();
+	}
+	
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@PostMapping
+	public Order create(@RequestBody Order order) {
+		return orderRepository.save(order);
 	}
 }
--- frontend/src/app/orders/order-form/order-form.component.ts
+++ frontend/src/app/orders/order-form/order-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { OrdersService } from '../services/orders.service';
 
@@ -14,7 +15,8 @@ export class OrderFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private service: OrdersService,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null],
@@ -27,14 +29,19 @@ export class OrderFormComponent implements OnInit {
 
   onSubmit(){
     this.service.save(this.form.value)
-    .subscribe(data => console.log(data), error => this.err());
+    .subscribe(data => this.sucess(), error => this.err());
   }
 
   onCancel(){
-
+    this.location.back();
   }
 
   private err(){
     this.snackBar.open("Erro ao salvar Pedido.", "", {duration: 3000});
+  }
+
+  private sucess(){
+    this.snackBar.open("Sucesso ao lançar Pedido!", "", {duration: 3000});
+    this.onCancel();
   }
 }
